0344dbb1fded8c0a3a61830101fb80e1e6b87c9b
JS-Tharun <tharunjs06@gmail.com>
2026-05-03 23:06:25 +0530

    Content Based recommendation created

--- app/Recommendation_System.py
+++ app/Recommendation_System.py
@@ -1,3 +1,59 @@
 import streamlit as st
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+
+st.set_page_config(
+    page_title="Intelligent Book Recommendation System",
+    layout="wide"
+)
+
+#--------------------------------------------------------------
+# Dataset Loading
+#--------------------------------------------------------------
+
+@st.cache_resource
+def load_data():
+    data = pd.read_csv("../datasets/cleaned_audible_catalog.csv")
+    return data
+
+df = load_data()
+
+
+
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf.fit_transform(df['Description'])
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
 
 st.write("# Book Recommendationt System")
+
+
+tab1, tab2 = st.tabs([
+    "Content-Based Recommendations",
+    "Genre-Based Recommendations"
+])
+
+book_choice = st.selectbox(
+    "Choose a book: ",
+    df['Book_Name'].tolist(),
+    index=None
+)
+
+if st.button("Get Recommendations"):
+    if book_choice == None:
+        st.warning("Please select a book to get recommendations.")
+    else:
+        matches = df[df['Book_Name'] == book_choice]
+        if len(matches) > 0:
+            idx = matches.index[0]
+            sim_scores = list(enumerate(cosine_sim[idx]))
+            sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+            book_indices = [i[0] for i in sim_scores[1:6]]
+            st.write(f"### Book Recommendations similar to ***{book_choice}***")
+            st.dataframe(
+                df.iloc[book_indices][['Book_Name','Author', 'Rating', 'Main_Genre']],
+                hide_index=True
+            )
+        else:
+            st.warning("Book not found ")
--- app/pages/1_EDA.py
+++ app/pages/1_EDA.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
+st.set_page_config(
+    page_title="Exploratory Data Analysis (EDA)"
+)
+
+#--------------------------------------------------------------
+# Dataset Loading
+#--------------------------------------------------------------
 @st.cache_resource
 def load_data():
     data = pd.read_csv("../datasets/cleaned_audible_catalog.csv")
@@ -8,7 +16,55 @@ def load_data():
 
 df = load_data()
 
+#--------------------------------------------------------------
+# Chart Functions
+#--------------------------------------------------------------
 
+def book_rating_distribution(df):
+    st.write("### Book Rating Distribution")
+    bins = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
+    labels = ['1-1.5', '1.5-2', '2-2.5', '2.5-3', '3-3.5', '3.5-4', '4-4.5', '4.5-5']
+    rating_bucket = pd.cut(df['Rating'], bins=bins, labels=labels, include_lowest=True)
+    rating_bucket_counts = rating_bucket.value_counts().sort_index().reset_index(name='Count')
+    st.bar_chart(
+        data=rating_bucket_counts,
+        x='Rating',
+        y='Count'
+    )
+
+def top_genres(df):
+    popular_genres = (
+        df['Main_Genre']
+        .value_counts()
+        .reset_index(name='Count')
+        .head(10)
+    )
+
+    st.write("### Top 10 Genres")
+    fig = px.bar(
+        popular_genres,
+        x="Main_Genre",
+        y="Count",
+        category_orders={'Main_Genre': popular_genres['Main_Genre'].tolist()},
+    )
+    st.plotly_chart(fig)
+
+
+def popular_books(df):
+    df_extended = df.copy()
+    df_extended['Popularity'] = df_extended['Rating'] * df_extended['Number_of_Reviews']
+    top_books = (
+        df_extended
+        .sort_values(by='Popularity', ascending=False)
+        .head(10)
+        [['Book_Name', 'Author', 'Rating', 'Number_of_Reviews']]
+    )
+    return top_books
+
+
+#--------------------------------------------------------------
+# EDA
+#--------------------------------------------------------------
 
 st.write("# Exploratory Data Analysis (EDA)")
 
@@ -21,5 +77,14 @@ with col2:
     with st.container(border=True):
         st.metric(label='Popular Genre', value=df['Main_Genre'].mode()[0])
 
-st.dataframe(df.head())
+with st.container(border=True):
+    st.write("### Popular Books")
+    st.dataframe(popular_books(df))
+
+with st.container(border=True):
+    book_rating_distribution(df)
+
+with st.container(border=True):
+    top_genres(df)
+
 
